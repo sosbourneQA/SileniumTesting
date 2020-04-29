cc99adb437474dcdea82c93e61b48194b5c00cdb
luke <lukesosbourne@gmail.com>
2020-04-29 14:14:20 +0100

    added a fluent wait method to AppTest

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertEquals;
@@ -49,5 +50,42 @@ public class AppTest
         driver.close();
     }
 
+
+
+    public class HouseBuilderTest {
+
+        private WebDriver driver;
+
+        @Before
+        public void setUp(){
+            driver = new ChromeDriver();
+        }
+
+        @Test
+        public void seleniumDrawHouse() throws InterruptedException {
+            driver.manage().window().maximize();
+            Thread.sleep(2000);
+            driver.get("https://www.youidraw.com/apps/painter/");
+            Thread.sleep(2000);
+
+            Actions action = new Actions(driver);
+            Thread.sleep(2000);
+
+            // draw house
+
+            action.moveByOffset( 400,400).clickAndHold().moveByOffset(400,0).perform();
+            Thread.sleep(2000);
+
+            action.moveByOffset( 830,400).clickAndHold().moveByOffset(400,0).perform();
+            Thread.sleep(2000);
+
+            action.moveByOffset( 830,600).clickAndHold().moveByOffset(400,0).perform();
+            Thread.sleep(2000);
+
+            action.moveByOffset( 400,600).clickAndHold().moveByOffset(400,0).perform();
+            Thread.sleep(2000);
+
+        }
+    }
 
 }
