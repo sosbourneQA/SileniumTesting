268b28389d0872aa7eddab03e1596b03450bdf8c
luke <lukesosbourne@gmail.com>
2020-04-29 13:58:19 +0100

    added a fluent wait method

--- src/test/java/org/example/WaitFunctions.java
+++ src/test/java/org/example/WaitFunctions.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import com.google.common.base.Function;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertTrue;
+
+
+
 public class WaitFunctions {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void fluentWaitExample() {
+        driver.get("http://www.google.com");
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(30, SECONDS)
+                .pollingEvery(5, SECONDS)
+                .ignoring(NoSuchElementException.class);
+        WebElement searchBar = wait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver webDriver) {
+                return driver.findElement(By.name("q"));
+            }
+        });
+        assertTrue(searchBar.isDisplayed());
+    }
 }
